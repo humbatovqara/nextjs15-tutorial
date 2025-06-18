ad9624caeb70f14dc69a6d56c185c21138870bab
Gara Humbatov <ghumbatov@rth.gov.az>
2025-06-18 09:32:21 +0400

    Link component (Tutorial-19)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Main Route, Home page</h1>;
+  return (
+    <>
+      <h1>Welcome Home!</h1>
+      <Link href="/about">About</Link>
+      <Link href="/products">Products</Link>
+    </>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,28 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId = 100;
+
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Product list</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link href="/products/1">Product 1</Link>
+        </li>
+        <li>
+          <Link href="/products/2">Product 2</Link>
+        </li>
+        <li>
+          {/* Replace - clear history */}
+          <Link href="/products/3" replace>
+            Product 3
+          </Link>
+        </li>
+        <li>
+          <Link href={`/products/${productId}`}>Product {productId}</Link>
+        </li>
       </ul>
     </>
   );

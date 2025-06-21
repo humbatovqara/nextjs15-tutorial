51b1d5025503da7e947395d984807d8588eb6a87
Gara Humbatov <ghumbatov@rth.gov.az>
2025-06-21 17:46:04 +0400

    Loading UI (Tutorial-24)

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -5,6 +5,12 @@ async function UsersPage() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await res.json();
 
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("international delay");
+    }, 2000);
+  });
+
   return (
     <div className="flex flex-col w-md m-auto">
       <h1>Users</h1>

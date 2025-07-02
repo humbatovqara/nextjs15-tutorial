4c0c276b33529808c57035c2745d14cea667203f
Gara Humbatov <ghumbatov@rth.gov.az>
2025-07-02 10:58:57 +0400

    Parallel Routes (Tutorial-30)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { ErrorWrapper } from "./error-wrapper";
+// import { ErrorWrapper } from "./error-wrapper";
 
 export const metadata: Metadata = {
   title: {
@@ -26,7 +26,8 @@ export default function RootLayout({
         >
           <p>Header</p>
         </header>
-        <ErrorWrapper>{children}</ErrorWrapper>
+        {/* <ErrorWrapper>{children}</ErrorWrapper> */}
+        {children}
         <footer
           style={{
             backgroundColor: "ghostwhite",
